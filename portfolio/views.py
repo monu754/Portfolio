72b542c63680d1b1e0c2b714223267058f4873b8
Manotosh <m.mandal20048295@gmail.com>
2025-07-30 18:43:54 +0530

    Initial Message

---
+++ portfolio/views.py
@@ -0,0 +1,23 @@
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
+
+def home(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            try:
+                send_mail(
+                    form.cleaned_data['subject'],
+                    f"From: {form.cleaned_data['name']} <{form.cleaned_data['email']}>\n\n{form.cleaned_data['message']}",
+                    form.cleaned_data['email'],
+                    ['m.mandal20048295@gmail.com']
+                )
+                messages.success(request, "Message sent successfully!")
+            except:
+                messages.error(request, "Something went wrong while sending the message.")
+            return redirect('home')
+
+    return render(request, 'index.html', {'form': form})
